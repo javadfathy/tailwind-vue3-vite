2bcb1a935a8e62cca41afa683093a125a84cba03
javad <jwadfthy2@gmail.com>
2023-11-01 14:43:16 +0330

    add typescript

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
 
 defineProps({
   msg: String,
 })
 
-const count = ref(0)
+const count = ref<Number>(0)
 </script>
 
 <template>
